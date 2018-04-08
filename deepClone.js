0ed08e68ceddd8d20565b730e664182dee1f1aa7
QiandaoLakeYY <18368493653@163.com>
2018-04-08 10:21:46 +0800

    feat:add inherit

--- deepClone.js
+++ deepClone.js
@@ -1,12 +1,18 @@
 // for (let j = 0,len = arr.length; j < len; j++)
-function deepClone (originVal) {
+function deepClone (originVal, isSaveConstructor = false) {
 	let temp = originVal instanceof Array ? [] : {}
 
+	if (isSaveConstructor) {
+		temp.__proto__ = originVal
+	}
+	
 	for (let item in originVal) {
-		if (isReferenceType(originVal[item])) {
-			temp[item] = deepClone(originVal[item])
-		} else {
-			temp[item] = originVal[item]
+		if (originVal.hasOwnProperty(item)) {
+			if (isReferenceType(originVal[item])) {
+				temp[item] = deepClone(originVal[item])
+			} else {
+				temp[item] = originVal[item]
+			}
 		}
 	}
 	return temp
@@ -16,11 +22,3 @@ function isReferenceType (val) {
 	return Object.prototype.toString.call(val) === '[object Object]' ||
 	       Object.prototype.toString.call(val) === '[object Array]'
 }  
-
-function isArray (val) {
-	return Object.prototype.toString.call(val) === '[object Array]'
-}
-
-function isObject (val) {
-	return Object.prototype.toString.call(val) == '[object Object]'
-}
